13ed72c7ea0ef68dd3277702655128b738933aa3
Bernardo <programming@bernardomg.com>
2021-10-02 00:07:00 +0200

    Simplified controller API

--- src/main/java/com/bernardomg/darksouls/explorer/graph/controller/GraphController.java
+++ src/main/java/com/bernardomg/darksouls/explorer/graph/controller/GraphController.java
@@ -3,9 +3,7 @@ package com.bernardomg.darksouls.explorer.graph.controller;
 
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,28 +27,18 @@ public class GraphController {
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Graph> read(
-            @RequestParam(value = "links") final List<String> links) {
-        final Graph graph;
-
-        graph = service.getGraph(links);
-        return new ResponseEntity<>(graph, HttpStatus.OK);
+    public Graph read(@RequestParam(value = "links") final List<String> links) {
+        return service.getGraph(links);
     }
 
     @GetMapping(path = "/links", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Iterable<String>> readLinks() {
-        final Iterable<String> links;
-
-        links = service.getLinks();
-        return new ResponseEntity<>(links, HttpStatus.OK);
+    public Iterable<String> readLinks() {
+        return service.getLinks();
     }
 
     @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Info> readNode(@PathVariable final Long id) {
-        final Info info;
-
-        info = service.getInfo(id);
-        return new ResponseEntity<>(info, HttpStatus.OK);
+    public Info readNode(@PathVariable final Long id) {
+        return service.getInfo(id);
     }
 
 }
