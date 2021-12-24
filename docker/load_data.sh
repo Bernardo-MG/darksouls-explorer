cc9e4b2d6abeafe8e5265f4cd400e793455b4503
Bernardo <programming@bernardomg.com>
2021-12-24 16:14:08 +0100

    Corrected data cleanup

--- docker/load_data.sh
+++ docker/load_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cypher-shell -a bolt://graph-db:7687 --format plain < 'MATCH(n) DELETE n';
+cypher-shell -a bolt://graph-db:7687 --format plain < /ds/cleanup.cypher;
 
 cypher-shell -a bolt://graph-db:7687 --format plain < /ds/darksouls_1/import.cypher;
 
