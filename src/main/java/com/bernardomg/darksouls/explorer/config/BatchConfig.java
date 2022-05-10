1c77d59c561dc5669992961368769f0a7693c7c7
Bernardo <programming@bernardomg.com>
2022-05-10 23:20:06 +0200

    Fixed batch armor load

--- src/main/java/com/bernardomg/darksouls/explorer/config/BatchConfig.java
+++ src/main/java/com/bernardomg/darksouls/explorer/config/BatchConfig.java
@@ -13,8 +13,8 @@ import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
-import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
@@ -26,7 +26,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 
 import com.bernardomg.darksouls.explorer.batch.DBLogProcessor;
-import com.bernardomg.darksouls.explorer.item.armor.domain.DtoArmor;
+import com.bernardomg.darksouls.explorer.batch.model.ArmorBatchData;
 
 @Configuration
 @EnableBatchProcessing
@@ -58,47 +58,48 @@ public class BatchConfig {
     }
 
     @Bean("armorItemReader")
-    public ItemReader<DtoArmor>
-            getArmorItemReader(final LineMapper<DtoArmor> lineMapper) {
-        final FlatFileItemReader<DtoArmor> itemReader;
-
-        itemReader = new FlatFileItemReader<DtoArmor>();
-
-        itemReader.setLineMapper(lineMapper);
-        itemReader.setLinesToSkip(1);
-        itemReader.setResource(armorsData);
-
-        return itemReader;
+    public ItemReader<ArmorBatchData>
+            getArmorItemReader(final LineMapper<ArmorBatchData> lineMapper) {
+        return new FlatFileItemReaderBuilder<ArmorBatchData>()
+            .name("armorItemReader")
+            .resource(armorsData)
+            .delimited()
+            .names(
+                new String[] { "name", "description", "weight", "durability" })
+            .linesToSkip(1)
+            .lineMapper(lineMapper)
+            .build();
     }
 
     @Bean("armorItemWriter")
-    public ItemWriter<DtoArmor> getArmorItemWriter() {
-        final JdbcBatchItemWriter<DtoArmor> itemWriter;
+    public ItemWriter<ArmorBatchData> getArmorItemWriter() {
+        final JdbcBatchItemWriter<ArmorBatchData> itemWriter;
 
-        itemWriter = new JdbcBatchItemWriter<DtoArmor>();
+        itemWriter = new JdbcBatchItemWriter<ArmorBatchData>();
 
         itemWriter.setDataSource(datasource);
         itemWriter.setSql(
             "INSERT INTO armors (name, description, weight, durability) VALUES (:name, :description, :weight, :durability)");
         itemWriter.setItemSqlParameterSourceProvider(
-            new BeanPropertyItemSqlParameterSourceProvider<DtoArmor>());
+            new BeanPropertyItemSqlParameterSourceProvider<ArmorBatchData>());
 
         return itemWriter;
     }
 
     @Bean("armorLineMapper")
-    public LineMapper<DtoArmor> getArmorLineMapper() {
+    public LineMapper<ArmorBatchData> getArmorLineMapper() {
         final DelimitedLineTokenizer lineTokenizer;
-        final BeanWrapperFieldSetMapper<DtoArmor> fieldSetMapper;
-        final DefaultLineMapper<DtoArmor> lineMapper;
+        final BeanWrapperFieldSetMapper<ArmorBatchData> fieldSetMapper;
+        final DefaultLineMapper<ArmorBatchData> lineMapper;
 
-        lineMapper = new DefaultLineMapper<DtoArmor>();
+        lineMapper = new DefaultLineMapper<ArmorBatchData>();
 
         lineTokenizer = new DelimitedLineTokenizer();
-        lineTokenizer.setNames(new String[] { "name" });
-        lineTokenizer.setIncludedFields(new int[] { 0 });
-        fieldSetMapper = new BeanWrapperFieldSetMapper<DtoArmor>();
-        fieldSetMapper.setTargetType(DtoArmor.class);
+        lineTokenizer.setNames(
+            new String[] { "name", "description", "weight", "durability" });
+        lineTokenizer.setIncludedFields(new int[] { 0, 2, 3, 4 });
+        fieldSetMapper = new BeanWrapperFieldSetMapper<ArmorBatchData>();
+        fieldSetMapper.setTargetType(ArmorBatchData.class);
 
         lineMapper.setLineTokenizer(lineTokenizer);
         lineMapper.setFieldSetMapper(fieldSetMapper);
@@ -107,10 +108,10 @@ public class BatchConfig {
     }
 
     @Bean("armorLoaderStep")
-    public Step getArmorLoaderStep(final ItemReader<DtoArmor> reader,
-            final ItemWriter<DtoArmor> writer) {
+    public Step getArmorLoaderStep(final ItemReader<ArmorBatchData> reader,
+            final ItemWriter<ArmorBatchData> writer) {
         return stepBuilderFactory.get("step")
-            .<DtoArmor, DtoArmor> chunk(5)
+            .<ArmorBatchData, ArmorBatchData> chunk(5)
             .reader(reader)
             .processor(new DBLogProcessor<>())
             .writer(writer)
